400a95bc7431526aa7347469c6c00782cf675177
ihyavorskyi <ihor201324@gmail.com>
2020-11-23 10:41:52 +0200

    commit

--- assets/Script/GameMain.ts
+++ assets/Script/GameMain.ts
@@ -61,36 +61,37 @@ export default class GameMain extends cc.Component {
 
     onLoad() {
         this.button1.node.on('click', () => {
-            this.buttonCallback(1, this.button1,this.label1)
+            this.buttonCallback(1, this.button1, this.label1)
         }, this);
         this.button2.node.on('click', () => {
-            this.buttonCallback(1, this.button2,this.label2)
+            this.buttonCallback(2, this.button2, this.label2)
         }, this);
         this.button3.node.on('click', () => {
-            this.buttonCallback(1, this.button3,this.label3)
+            this.buttonCallback(3, this.button3, this.label3)
         }, this);
         this.button4.node.on('click', () => {
-            this.buttonCallback(1, this.button4,this.label4)
+            this.buttonCallback(4, this.button4, this.label4)
         }, this);
         this.button5.node.on('click', () => {
-            this.buttonCallback(1, this.button5,this.label5)
+            this.buttonCallback(5, this.button5, this.label5)
         }, this);
         this.button6.node.on('click', () => {
-            this.buttonCallback(1, this.button6,this.label6)
+            this.buttonCallback(6, this.button6, this.label6)
         }, this);
         this.button7.node.on('click', () => {
-            this.buttonCallback(1, this.button7,this.label7)
+            this.buttonCallback(7, this.button7, this.label7)
         }, this);
         this.button8.node.on('click', () => {
-            this.buttonCallback(1, this.button8,this.label8)
+            this.buttonCallback(8, this.button8, this.label8)
         }, this);
         this.button9.node.on('click', () => {
-            this.buttonCallback(1, this.button9,this.label9)
+            this.buttonCallback(9, this.button9, this.label9)
         }, this);
     }
 
-    buttonCallback(id: number, button: cc.Button,label: cc.Label) {
+    buttonCallback(id: number, button: cc.Button, label: cc.Label) {
         label.string = "X";
+        console.log(id);
         button.enabled = false;
     }
 }
