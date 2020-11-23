eaafef8c511ed9d14acb0947040600ae01c8c6e4
ihyavorskyi <ihor201324@gmail.com>
2020-11-23 12:01:39 +0200

    commit

--- assets/Script/GameMain.ts
+++ assets/Script/GameMain.ts
@@ -1,97 +1,48 @@
+import GameFacade from "./GameFacade";
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class GameMain extends cc.Component {
 
-    @property(cc.Button)
-    button1: cc.Button = null;
+    game: GameFacade;
 
-    @property(cc.Button)
-    button2: cc.Button = null;
-
-    @property(cc.Button)
-    button3: cc.Button = null;
-
-    @property(cc.Button)
-    button4: cc.Button = null;
-
-    @property(cc.Button)
-    button5: cc.Button = null;
-
-    @property(cc.Button)
-    button6: cc.Button = null;
-
-    @property(cc.Button)
-    button7: cc.Button = null;
-
-    @property(cc.Button)
-    button8: cc.Button = null;
-
-    @property(cc.Button)
-    button9: cc.Button = null;
-
-
-    @property(cc.Label)
-    label1: cc.Label = null;
-
-    @property(cc.Label)
-    label2: cc.Label = null;
-
-    @property(cc.Label)
-    label3: cc.Label = null;
-
-    @property(cc.Label)
-    label4: cc.Label = null;
-
-    @property(cc.Label)
-    label5: cc.Label = null;
-
-    @property(cc.Label)
-    label6: cc.Label = null;
-
-    @property(cc.Label)
-    label7: cc.Label = null;
-
-    @property(cc.Label)
-    label8: cc.Label = null;
-
-    @property(cc.Label)
-    label9: cc.Label = null;
-
+    buttonArray: cc.Button[] = [];
+    labelArray: cc.Label[] = [];
 
     onLoad() {
-        this.button1.node.on('click', () => {
-            this.buttonCallback(1, this.button1, this.label1)
-        }, this);
-        this.button2.node.on('click', () => {
-            this.buttonCallback(2, this.button2, this.label2)
-        }, this);
-        this.button3.node.on('click', () => {
-            this.buttonCallback(3, this.button3, this.label3)
-        }, this);
-        this.button4.node.on('click', () => {
-            this.buttonCallback(4, this.button4, this.label4)
-        }, this);
-        this.button5.node.on('click', () => {
-            this.buttonCallback(5, this.button5, this.label5)
-        }, this);
-        this.button6.node.on('click', () => {
-            this.buttonCallback(6, this.button6, this.label6)
-        }, this);
-        this.button7.node.on('click', () => {
-            this.buttonCallback(7, this.button7, this.label7)
-        }, this);
-        this.button8.node.on('click', () => {
-            this.buttonCallback(8, this.button8, this.label8)
-        }, this);
-        this.button9.node.on('click', () => {
-            this.buttonCallback(9, this.button9, this.label9)
-        }, this);
+
+        this.loadButtons();
+
+        for (let i = 0, j = 1; i < this.buttonArray.length; i++, j++) {
+            this.buttonArray[i].node.on('click', () => {
+                this.buttonCallback(j, this.buttonArray[i], this.labelArray[i])
+            }, this);
+        }
+        this.game = new GameFacade;
     }
 
     buttonCallback(id: number, button: cc.Button, label: cc.Label) {
-        label.string = "X";
-        console.log(id);
+        var player = this.game.GoStep(id);
+        player == 1 ? label.string = "X" : label.string = "O";
+
         button.enabled = false;
+
+        let isWin = this.game.CheckWin();
+        console.log(isWin);
+        
+        isWin == 1 ? console.log('X WIN') : isWin == 2 ? console.log('O WIN')
+            : console.log('not win yet....');
+    }
+
+
+    loadButtons() {
+        for (let i = 1; i < 10; i++) {
+
+            let button = cc.find(`Canvas/Button${i}`).getComponent(cc.Button);
+            this.buttonArray.push(button);
+
+            let label = cc.find(`Canvas/Button${i}/Background/Label${i}`).getComponent(cc.Label);
+            this.labelArray.push(label);
+        }
     }
 }
